bd5dd016b99a861a4361d19228759e5f774b0a81
Simon_Hsia <1353580458@qq.com>
2023-09-08 15:07:25 +0800

    重新配置了publicPath为./

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  //代理跨域,前端解决跨域问题
+  //  代理跨域,前端解决跨域问题
   /* devserver: {
     proxy: {
       '/api': {
@@ -11,5 +11,6 @@ module.exports = defineConfig({
     }
   } */
   // 项目打包上线后JS文件夹会有很多个map文件，如果运行报错，是不知道哪行的bug的，但有了map文件就可以准确地输出哪行哪列有错，但已经打包了就没必要再用了，不在开发阶段解决等啥呢
-  productionSourceMap: false
+  productionSourceMap: false,
+  publicPath: './'
 })
